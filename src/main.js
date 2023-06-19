4188ef072997971da191c45b8e187206d2640ceb
Koudu <gorb.andrey.08@gmail.com>
2023-06-19 11:25:29 +0300

    add setup project

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+// const arr=[8, 1,4, 11, 7,9, 9];
+// console.log(arr.sort((a, b) => a - b));
+
+// const arrToSet = new Set(arr);
+// console.log(arrToSet);
+
+// const setToArr = [...arrToSet]
+
+// const btn = document.getElementById("bt-1");
+// const btn2 = document.getElementById("bt-2");
+// const dialog = document.querySelector(".dialog");
+// //console.log(btn);
+
+// function click(e) {
+//     const input = document.getElementById("text");
+//     const text = document.getElementById("dialog-text");
+//     if (dialog && text) {
+//         text.innerHTML = input.value;
+//         dialog.classList.add("active");
+//     }
+// }
+
+
+// btn.addEventListener("click", click)
+// btn2?.addEventListener("click", () => {
+//     dialog?.classList.remove("active");
+// })
